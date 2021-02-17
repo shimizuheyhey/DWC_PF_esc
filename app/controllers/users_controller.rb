1519e734b4fdfc2df2a924e6c0c853deeea85a8b
shimizuheyhey <tetrisranner@gmail.com>
2021-02-17 18:08:28 +0000

    break

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,6 +13,7 @@ class UsersController < ApplicationController
   def show
    @user = User.find(params[:id])
    @bookmarks = Bookmark.where(user_id: current_user.id)
+   @post = @user.posts
   end
 
   def bookmark
