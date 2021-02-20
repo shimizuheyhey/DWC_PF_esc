6e681b0ab4ca18cfad505e8a01172d47766e48ea
shimizuheyhey <tetrisranner@gmail.com>
2021-02-20 17:46:37 +0000

    break

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,6 +18,19 @@ class UsersController < ApplicationController
 
   def bookmark
     @bookmarks = Bookmark.where(user_id: current_user.id)
+    total_second = 0
+    @bookmarks.each do |bookmark|
+      cut_time = bookmark.post.cut_times.first
+
+      total_second += cut_time.minute * 60
+      total_second += cut_time.second
+    end
+
+    @minute = total_second / 60
+    @second = total_second % 60
+     # @bookmarks.cut_times.each do |cut_time|
+       # @total_time << cut_time.minute * bookmark.count
+      #end
   end
 
   def update
