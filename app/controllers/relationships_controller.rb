95d0b31e82d8b8df87497520c09fd890b9eee507
shimizuheyhey <tetrisranner@gmail.com>
2021-02-25 04:18:22 +0000

    finish

--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,11 +2,11 @@ class RelationshipsController < ApplicationController
   before_action :authenticate_user!
   def create
     current_user.follow(params[:user_id])
-    redirect_to request.referer
+    #redirect_to request.referer
   end
   def destroy
     current_user.unfollow(params[:user_id])
-    redirect_to request.referer
+    #redirect_to request.referer
   end
 
   def followings
